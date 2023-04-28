b20eec56a7c4a9ea6170556a8cd54f817cf23da1
Александр Бриль <s.bril03@yandex.ru>
2023-04-28 11:59:04 +0500

    1.0

--- art_map/art_map/settings.py
+++ art_map/art_map/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'map.apps.MapConfig',
+    'leaflet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
