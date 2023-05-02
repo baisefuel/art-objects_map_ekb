63f96574b91cde8316b03c0dc6078dbda150753b
Александр Бриль <s.bril03@yandex.ru>
2023-05-02 21:25:54 +0500

    1.0

--- art_map/art_map/settings.py
+++ art_map/art_map/settings.py
@@ -30,7 +30,6 @@ if os.name == 'nt':
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -145,6 +144,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = ''
