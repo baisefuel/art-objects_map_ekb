9b98d6bd95dce987f3cfafd392208c55d93e41dd
Александр Бриль <s.bril03@yandex.ru>
2023-05-26 04:19:08 +0500

    1.0

--- art_map/art_map/urls.py
+++ art_map/art_map/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from map.views import map_view, about
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', map_view, name='map'),
     path('about/', about, name='about'),
 ]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
+
